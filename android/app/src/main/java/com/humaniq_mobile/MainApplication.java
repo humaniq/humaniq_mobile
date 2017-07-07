41b769b5e58b3bf71bb67a22b5434e1a379c80a7
blackPeanut <daniel@galt.hk>
2017-07-07 21:50:56 +0300

    add react-native-device-info

--- android/app/src/main/java/com/humaniq_mobile/MainApplication.java
+++ android/app/src/main/java/com/humaniq_mobile/MainApplication.java
@@ -3,6 +3,7 @@ package com.humaniq_mobile;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.brentvatne.react.ReactVideoPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNDeviceInfo(),
             new LinearGradientPackage(),
             new ReactVideoPackage(),
             new RNFetchBlobPackage(),
