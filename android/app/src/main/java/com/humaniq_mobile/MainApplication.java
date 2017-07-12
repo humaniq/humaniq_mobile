af85c5654bf2015d495be1559e90f22b89803831
dimmetrius <topsoft@Dimmetrius.local>
2017-07-12 18:43:50 +0300

    multiplex support

--- android/app/src/main/java/com/humaniq_mobile/MainApplication.java
+++ android/app/src/main/java/com/humaniq_mobile/MainApplication.java
@@ -18,11 +18,12 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import android.support.multidex.MultiDexApplication;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
+public class MainApplication extends MultiDexApplication implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
