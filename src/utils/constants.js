1cddf640a18b7b2367205ff84f88d638cdf419ab
Daniel <daniel@galt.hk>
2017-07-13 10:50:06 +0300

    Revert "Profile"

--- src/utils/constants.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by root on 7/10/17.
- */
-export const HEADER_TRANSLATE = 'header_translate';
-export const HEADER_TRANSLATE2 = 'header_translate2';
-export const VIEW_TRANSLATE = 'view_translate';
-export const VIEWY_TRANSLATE = 'viewy_translate';
-export const IMAGE_OPACITY = 'image_opacity';
