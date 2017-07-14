3052379a494348b73ce8ce97f0b2e2b2bd3f0d99
NikitaSergeevich <dercoebo@mail.ru>
2017-07-14 23:23:40 +0300

    Keyboard styling and desired icons

--- src/components/Shared/Components/Keyboard.js
+++ src/components/Shared/Components/Keyboard.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 import {
-    Text,
-    View,
-    TouchableOpacity,
-    TouchableHighlight,
-    Image,
+  Text,
+  View,
+  TouchableOpacity,
+  TouchableHighlight,
+  Image,
 } from 'react-native';
 import PropTypes from 'prop-types';
 import CustomStyleSheet from '../../../utils/customStylesheet';
 
-const ic_help = require('../../../assets/icons/ic_help.png');
+const ic_help = require('../../../assets/icons/chat_white.png');
+const back_space_white = require('../../../assets/icons/back_space_white.png');
 
 export default class VirtualKeyboard extends Component {
   static propTypes = {
     color: PropTypes.string,
+    isBackspaceEnabled: PropTypes.bool,
     onNumberPress: PropTypes.func.isRequired,
     onBackspacePress: PropTypes.func.isRequired,
     onHelpPress: PropTypes.func.isRequired,
@@ -39,11 +41,11 @@ export default class VirtualKeyboard extends Component {
           middleCellValues.includes(number) && styles.middleCell,
         ]}
         key={number}
-        accessibilityLabel={number.toString()}
+        accessibilityLabel={number.toString() }
         onPress={() => {
           this.props.onNumberPress(number.toString());
-        }}
-      >
+        } }
+        >
         <Text style={[styles.number]}>{number}</Text>
       </TouchableHighlight >
     );
@@ -53,31 +55,30 @@ export default class VirtualKeyboard extends Component {
     <TouchableOpacity
       style={styles.backspace}
       onPress={this.props.onBackspacePress}
-    >
-      <Text>back</Text>
+      >
+      {this.props.isBackspaceEnabled && <Image source={back_space_white} />}
     </TouchableOpacity>
-        );
+  );
 
   renderHelp = () => (
     <TouchableOpacity
       style={styles.help}
       onPress={this.props.onHelpPress}
-    >
+      >
       <Image source={ic_help} />
     </TouchableOpacity>
-        );
+  );
 
   render() {
     return (
       <View style={styles.container}>
-        {this.renderRow([1, 2, 3])}
-        {this.renderRow([4, 5, 6])}
-        {this.renderRow([7, 8, 9])}
-        {this.renderRow([7, 8, 9])}
+        {this.renderRow([1, 2, 3]) }
+        {this.renderRow([4, 5, 6]) }
+        {this.renderRow([7, 8, 9]) }
         <View style={styles.row}>
-          {this.renderHelp()}
-          {this.renderCell(0)}
-          {this.renderBackspace()}
+          {this.renderHelp() }
+          {this.renderCell(0) }
+          {this.renderBackspace() }
         </View>
       </View>
     );
@@ -88,15 +89,15 @@ const styles = CustomStyleSheet({
   container: {
     alignSelf: 'center',
     height: 224,
-    width: 216,
-    backgroundColor: 'transparent'
+    width: 230,
   },
   row: {
+    flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between'
   },
   number: {
-    fontSize: 25,
+    fontSize: 30,
     textAlign: 'center',
     color: '$cPaper'
   },
@@ -104,16 +105,18 @@ const styles = CustomStyleSheet({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    marginRight: 10,
   },
   help: {
     flex: 1,
+    marginLeft: 6,
     justifyContent: 'center',
     alignItems: 'center',
   },
   cell: {
-    height: 40,
-    width: 40,
-    borderRadius: 18,
+    flex: 1,
+    marginHorizontal: 12,
+    borderRadius: 100,
     justifyContent: 'center',
   },
   middleCell: {
