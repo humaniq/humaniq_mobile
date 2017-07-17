fbdcb5d9d04652e3ce15352312a3469bcf9837c9
djamik123 <djurayev.d@gmail.com>
2017-07-17 09:30:30 +0500

    eslint fix

--- setup.js
+++ setup.js
@@ -2,36 +2,30 @@
  * Created by root on 6/28/17.
  */
 
-jest.mock('react-native-camera', () => {
-    return {
-        constants:  {
-            Aspect: {},
-            BarCodeType: {},
-            Type: {},
-            CaptureMode: {},
-            CaptureTarget: {},
-            CaptureQuality: {},
-            Orientation: {},
-            FlashMode: {},
-            TorchMode: {}
-        }
-    }
-});
+jest.mock('react-native-camera', () => ({
+  constants: {
+    Aspect: {},
+    BarCodeType: {},
+    Type: {},
+    CaptureMode: {},
+    CaptureTarget: {},
+    CaptureQuality: {},
+    Orientation: {},
+    FlashMode: {},
+    TorchMode: {},
+  },
+}));
 
-jest.mock('react-native-network-info', () => 'DeviceInfo')
+jest.mock('react-native-network-info', () => 'DeviceInfo');
 //
-jest.mock('react-native-fetch-blob', () => {
-    return {
-        DocumentDir: () => {},
-    }
-});
+jest.mock('react-native-fetch-blob', () => ({
+  DocumentDir: () => {},
+}));
 
-jest.mock('Linking', () => {
-    return {
-        addEventListener: jest.fn(),
-        removeEventListener: jest.fn(),
-        openURL: jest.fn(),
-        canOpenURL: jest.fn(),
-        getInitialURL: jest.fn(),
-    }
-})
+jest.mock('Linking', () => ({
+  addEventListener: jest.fn(),
+  removeEventListener: jest.fn(),
+  openURL: jest.fn(),
+  canOpenURL: jest.fn(),
+  getInitialURL: jest.fn(),
+}));
