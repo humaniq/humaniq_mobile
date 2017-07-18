3d929bda3a50d670f7aa372b64eb84e304f9687e
NikitaSergeevich <dercoebo@mail.ru>
2017-07-19 02:06:03 +0300

    sizes, icons and header

--- src/components/Login/CountryCode.js
+++ src/components/Login/CountryCode.js
@@ -10,23 +10,37 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import CustomStyleSheet from '../../utils/customStylesheet';
 
-//const icHelp = require('../../../assets/icons/chat_white.png');
-//const backSpaceWhite = require('../../../assets/icons/back_space_white.png');
+const backWhite = require('../../assets/icons/back_white.png');
+const searchWhite = require('../../assets/icons/search_white.png');
 
 export class CountryCode extends Component {
-  static propTypes = {
+    static propTypes = {
 
-    navigation: PropTypes.shape({
-      navigate: PropTypes.func.isRequired,
-      dispatch: PropTypes.func.isRequired,
-      state: PropTypes.object,
-    }),
-  };
+        navigation: PropTypes.shape({
+            navigate: PropTypes.func.isRequired,
+            dispatch: PropTypes.func.isRequired,
+            state: PropTypes.object,
+        }),
+    };
 
     renderSection = (letter, countryarray) => {
         return (
-            <View>
-            </View>
+            [<View style={styles.section}>
+                <View style={{ width: 64, backgroundColor: 'red' }}>
+                </View>
+                <View style={{ width: 296}}>
+                    <View style={{ height: 56, backgroundColor: 'red' }}/>
+                    <View style={{ height: 56 }}/>
+                </View>
+            </View> ,
+            <View style={styles.section}>
+                <View style={{ width: 64, backgroundColor: 'red' }}>
+                </View>
+                <View style={{ width: 296}}>
+                    <View style={{ height: 56, backgroundColor: 'red' }}/>
+                    <View style={{ height: 56 }}/>
+                </View>
+            </View>]
         );
     };
 
@@ -34,9 +48,18 @@ export class CountryCode extends Component {
         return (
             <View style={styles.container}>
                 <View style={styles.header}>
+                    <TouchableOpacity style={styles.backButton}>
+                        <Image source={backWhite}/>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.searchButton}>
+                        <Image source={searchWhite}/>
+                    </TouchableOpacity>
                 </View>
                 <View style={styles.row}>
                     <ScrollView style={styles.scroll}>
+                        {
+                            this.renderSection()
+                        }
                     </ScrollView>
                 </View>
             </View>
@@ -45,7 +68,7 @@ export class CountryCode extends Component {
 }
 
 const mapStateToProps = state => ({
-  accounts: state.accounts,
+    accounts: state.accounts,
 });
 
 export default connect(mapStateToProps)(CountryCode);
@@ -53,16 +76,31 @@ export default connect(mapStateToProps)(CountryCode);
 const styles = CustomStyleSheet({
     container: {
         flex: 1,
-        backgroundColor: 'red'
     },
     header: {
+        flexDirection: 'row',
+        backgroundColor: '$cBrand_dark',
         height: 56,
+        justifyContent: 'space-between',
+    },
+    backButton: {
+        height: 24,
+        width: 24,
+        margin: 16
+    },
+    searchButton: {
+        height: 24,
+        width: 24,
+        margin: 16
+    },
+    section: {
+        flexDirection: 'row'
     },
     row: {
         flex: 1,
     },
     scroll: {
-
+        flex: 1,
     },
     number: {
         fontSize: 30,
