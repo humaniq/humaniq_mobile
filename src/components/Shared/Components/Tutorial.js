670ee8f91f5914f6019e27fba6e09682edaf0905
NikitaSergeevich <dercoebo@mail.ru>
2017-07-21 00:46:24 +0300

    illustrations added

--- src/components/Shared/Components/Tutorial.js
+++ src/components/Shared/Components/Tutorial.js
@@ -7,7 +7,9 @@ import CustomStyleSheet from '../../../utils/customStylesheet';
 import ConfirmButton from '../Buttons/ConfirmButton';
 import HelpButton from '../Buttons/HelpButton';
 // assets
-const illustration = require('../../../assets/icons/camera_illustration.png');
+const cameraIllustration = require('../../../assets/icons/camera_illustration.png');
+const passwordIllustration = require('../../../assets/icons/password_illustration.png');
+const telephoneIllustration = require('../../../assets/icons/telephone_illustration.png');
 // const play = require('../../../assets/icons/ic_play.png');
 
 export default class Tutorial extends Component {
@@ -34,23 +36,38 @@ export default class Tutorial extends Component {
   handleConfirmPress = () => {
     const navState = this.props.navigation.state;
     this.props.navigation.navigate(this.state.nextScene, { ...navState.params });
-  };
+};
 
-  handleHelpPress = () => {
-    this.props.navigation.navigate('Instructions');
-  };
+handleHelpPress = () => {
+  this.props.navigation.navigate('Instructions');
+};
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Image style={styles.illustration} source={illustration} />
-        <View style={styles.buttonsContainer}>
-          <HelpButton onPress={this.handleHelpPress} />
-          <ConfirmButton onPress={this.handleConfirmPress} />
-        </View>
-      </View>
-    );
+render() {
+  let source = "";
+  switch (this.state.nextScene) {
+    case "Camera":
+      source = cameraIllustration;
+      break;
+    case "Password":
+      source = passwordIllustration;
+      break;
+    case "TelInput":
+      source = telephoneIllustration;
+      break;
+    default:
+      break;
   }
+
+  return (
+    <View style={styles.container}>
+      <Image style={styles.illustration} source={source} />
+      <View style={styles.buttonsContainer}>
+        <HelpButton onPress={this.handleHelpPress} />
+        <ConfirmButton onPress={this.handleConfirmPress} />
+      </View>
+    </View>
+  );
+}
 }
 
 const styles = CustomStyleSheet({
