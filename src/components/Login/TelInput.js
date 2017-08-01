bff1af42869b99fca393c13da2d025e5ea15f9af
blackPeanut <daniel@galt.hk>
2017-08-01 12:12:24 +0300

    add saving number to primary account on telInput step

--- src/components/Login/TelInput.js
+++ src/components/Login/TelInput.js
@@ -111,6 +111,11 @@ export class TelInput extends Component {
 
   handlePhoneConfirm = () => {
     const phone_number = this.state.phone;
+    // *************************************
+    // temp to debug accounts layout (preview number);
+    // *************************************
+    this.props.savePhone(VMasker.toNumber(`${this.state.code}${phone_number}`));
+
     if (this.phonenumber(phone_number)) {
       this.props.phoneNumberCreate({
         account_id: this.props.user.account.payload.payload.account_information.account_id,
